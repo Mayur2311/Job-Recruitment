6072788d63122857a3f7de69607b3839557fc504
sruthi109 <sruthi.podapati@gmail.com>
2021-06-27 19:00:18 -0400

    Updated UserActivity.java file

--- app/src/main/java/com/bits/jobhunt/UserActivity.java
+++ app/src/main/java/com/bits/jobhunt/UserActivity.java
@@ -1,14 +1,85 @@
 package com.bits.jobhunt;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class UserActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavController navController;
+    NavigationView navigationView;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        setupNavigation();
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        item.setCheckable(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = item.getItemId();
+
+        if (id == R.id.dashboardFragment) {
+            navController.navigate(R.id.dashboardFragment);
+        } else if (id == R.id.logout) {
+            firebaseAuth.signOut();
+            Intent intent = new Intent(this, LogInActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        return true;
+    }
+
+    public void setupNavigation() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerDashLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navController = Navigation.findNavController(this, R.id.host_fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
