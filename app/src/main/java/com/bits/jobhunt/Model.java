3aaa8282be7bb36df1e9447f7d8ffd7859a4d596
ShivaPraneeth <shivapraneeth.praneeth@gmail.com>
2021-07-03 18:38:18 -0400

    retrieving of job list from firestore is done

---
+++ app/src/main/java/com/bits/jobhunt/Model.java
@@ -0,0 +1,91 @@
+package com.bits.jobhunt;
+
+public class Model {
+
+    String CompanyName,Description,JobName,JobType,Location,Qualifications,Salary,numberOFHires;
+
+    public Model() {
+    }
+
+    public Model(String companyName, String jobName, String location, String salary) {
+        CompanyName = companyName;
+        JobName = jobName;
+        Location = location;
+        Salary = salary;
+    }
+
+    public Model(String companyName, String description, String jobName, String jobType, String location, String qualifications, String salary, String numberOFHires) {
+        CompanyName = companyName;
+        Description = description;
+        JobName = jobName;
+        JobType = jobType;
+        Location = location;
+        Qualifications = qualifications;
+        Salary = salary;
+        this.numberOFHires = numberOFHires;
+    }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getJobName() {
+        return JobName;
+    }
+
+    public void setJobName(String jobName) {
+        JobName = jobName;
+    }
+
+    public String getJobType() {
+        return JobType;
+    }
+
+    public void setJobType(String jobType) {
+        JobType = jobType;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getQualifications() {
+        return Qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        Qualifications = qualifications;
+    }
+
+    public String getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(String salary) {
+        Salary = salary;
+    }
+
+    public String getNumberOFHires() {
+        return numberOFHires;
+    }
+
+    public void setNumberOFHires(String numberOFHires) {
+        this.numberOFHires = numberOFHires;
+    }
+}
