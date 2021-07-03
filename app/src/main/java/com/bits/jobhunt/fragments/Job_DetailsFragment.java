04657bd7a9c3d92e9beb1a73af01184cc7a89cd4
ShivaPraneeth <shivapraneeth.praneeth@gmail.com>
2021-07-03 18:38:31 -0400

    sssss

--- app/src/main/java/com/bits/jobhunt/fragments/Job_DetailsFragment.java
+++ app/src/main/java/com/bits/jobhunt/fragments/Job_DetailsFragment.java
@@ -2,59 +2,34 @@ package com.bits.jobhunt.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.bits.jobhunt.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Job_DetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Job_DetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView user_jobdetails_job_title, user_jobdetails_company_name, user_jobdetails_company_location, user_jobdetails_salaryinnumber, user_jobdetails_jobtype1, user_jobdetails_vacancynumber, user_jobdetails_qualificationdetail1, user_jobdetails_company_details;
+    Button user_jobdetails_apply, user_jobdetails_save;
 
     public Job_DetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Job_DetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Job_DetailsFragment newInstance(String param1, String param2) {
-        Job_DetailsFragment fragment = new Job_DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
@@ -62,5 +37,22 @@ public class Job_DetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_job__details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        user_jobdetails_job_title = view.findViewById(R.id.user_jobdetails_job_title);
+        user_jobdetails_company_name=view.findViewById(R.id.user_jobdetails_company_name);
+        user_jobdetails_company_location=view.findViewById(R.id.user_jobdetails_company_location);
+        user_jobdetails_salaryinnumber=view.findViewById(R.id.user_jobdetails_salaryinnumber);
+        user_jobdetails_jobtype1=view.findViewById(R.id.user_jobdetails_jobtype1);
+        user_jobdetails_vacancynumber=view.findViewById(R.id.user_jobdetails_vacancynumber);
+        user_jobdetails_qualificationdetail1=view.findViewById(R.id.user_jobdetails_qualificationdetail1);
+        user_jobdetails_company_details=view.findViewById(R.id.user_jobdetails_company_details);
+        user_jobdetails_apply=view.findViewById(R.id.user_jobdetails_apply);
+        user_jobdetails_save=view.findViewById(R.id.user_jobdetails_save);
+
+      /*  user_jobdetails_job_title.setText(getIntent().getStringExtra("JobName").toString());*/
     }
 }
