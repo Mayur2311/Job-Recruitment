0c75703eb9c5932f37c82f3e5a473eabf9469512
Abhishek <imaeke96@gmail.com>
2021-07-13 10:01:41 -0400

    checking account

--- app/src/main/java/com/bits/jobhunt/fragments/User_Added_Post_List.java
+++ app/src/main/java/com/bits/jobhunt/fragments/User_Added_Post_List.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import com.bits.jobhunt.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link User_Added_Post_List#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class User_Added_Post_List extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
