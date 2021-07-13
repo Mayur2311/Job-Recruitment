7679f4e9790a0cd91d39c34752b7a4ee56ceb10a
Abhishek <bit22bit>
2021-07-13 09:56:18 -0400

    edited

--- app/src/main/java/com/bits/jobhunt/fragments/User_Added_Post_List.java
+++ app/src/main/java/com/bits/jobhunt/fragments/User_Added_Post_List.java
@@ -30,24 +30,6 @@ public class User_Added_Post_List extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment User_Added_Post_List.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static User_Added_Post_List newInstance(String param1, String param2) {
-        User_Added_Post_List fragment = new User_Added_Post_List();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
