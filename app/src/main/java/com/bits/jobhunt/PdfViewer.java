52d55b503eddbc3635cd5cb880d5f511b222990d
Mayur2311 <mayurrpatel1997@gmail.com>
2021-08-21 00:58:38 -0400

    Pdf functionality has been added.

---
+++ app/src/main/java/com/bits/jobhunt/PdfViewer.java
@@ -0,0 +1,106 @@
+package com.bits.jobhunt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PdfViewer extends AppCompatActivity {
+
+    Toolbar toolbar;
+    String url;
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_viewer);
+
+        toolbar = findViewById(R.id.Pdftoolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        url = getIntent().getStringExtra("url");
+
+        pdfView = findViewById(R.id.pdfView);
+
+        new PdfDownload().execute(url);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home)
+        {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class PdfDownload extends AsyncTask<String, Void, InputStream>{
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                if (urlConnection.getResponseCode() == 200)
+                {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                }
+
+            }
+            catch (MalformedURLException e){
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream)
+                    .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                    .enableSwipe(true) // allows to block changing pages using swipe
+                    .swipeHorizontal(false)
+                    .enableDoubletap(true)
+                    .defaultPage(0)
+                    .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                    .password(null)
+                    .scrollHandle(null)
+                    .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                    // spacing between pages in dp. To define spacing color, set view background
+                    .spacing(0)
+                    .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                    .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
+                    .pageSnap(false) // snap pages to screen boundaries
+                    .pageFling(false) // make a fling change only a single page like ViewPager
+                    .nightMode(false) // toggle night mode
+                    .load();
+        }
+    }
+}
